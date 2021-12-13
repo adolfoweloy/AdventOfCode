965ec86eb0e2d88e869bed7d52083a99e2cd99fe
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2021-12-13 22:53:56 +1100

    removes repetition in this code

--- day_02.hs
+++ day_02.hs
@@ -1,33 +1,27 @@
 import Data.List
 import System.IO
 
-type Position   = (Int, Int)
-type Increment  = Int
-type Command    = (String, Increment)
 type Aim        = Int
-type Position'  = (Int, Int, Aim)
+type Position   = (Int, Int, Aim)
+type Command    = (String, Int)
+type NextPos    = (Command -> Position -> Position)
 
-nextPos :: Command -> Position -> Position
-nextPos (c, n) (h, d) = case c of
-  "forward" -> (h + n, d)
-  "down"    -> (h, d + n)
-  "up"      -> (h, d - n)
+nextPos :: NextPos 
+nextPos (c, n) (h, d, _) = case c of
+  "forward" -> (h + n, d, 0)
+  "down"    -> (h, d + n, 0)
+  "up"      -> (h, d - n, 0)
 
-nextPos' :: Command -> Position' -> Position'
+nextPos' :: NextPos 
 nextPos' (c, x) (h, d, a) = case c of
   "forward" -> (h + x, d + (a * x), a)
   "down"    -> (h, d, a + x)
   "up"      -> (h, d, a - x)
 
-dive :: [Command] -> Position -> Position
-dive []  p     = p
-dive [c] p     = nextPos c p 
-dive (c:xs) p  = dive xs (nextPos c p)
-
-dive' :: [Command] -> Position' -> Position'
-dive' [] p      = p
-dive' [c] p     = nextPos' c p
-dive' (c:xs) p  = dive' xs (nextPos' c p)
+dive :: [Command] -> Position -> NextPos -> Position
+dive []  p f    = p
+dive [c] p f    = f c p 
+dive (c:xs) p f = dive xs (f c p) f
 
 toi :: [Char] -> Int
 toi c = read c :: Int
@@ -37,17 +31,14 @@ parse []        = []
 parse (x:y:xs)  = (x, toi y):(parse xs)
 
 calc :: Position -> Int
-calc (x,y) = x * y
-
-calc' :: Position' -> Int
-calc' (x,y,_) = x * y
+calc (x,y,_) = x * y
 
 main = do
     contents <- readFile "day_02.input"
     let xs = parse $ words contents
     putStrLn "solution 1"
-    print $ calc $ dive xs (0, 0)
+    print $ calc $ dive xs (0, 0, 0) nextPos
 
     putStrLn "solution 2"
-    print $ calc' $ dive' xs (0, 0, 0)
+    print $ calc $ dive xs (0, 0, 0) nextPos'
 
