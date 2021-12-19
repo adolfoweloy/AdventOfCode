ad6d1b47288ede4fa1c23ec6754f6a8b3eaab9ad
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2021-12-19 20:28:56 +1100

    day 03

--- day_03.hs
+++ day_03.hs
@@ -2,10 +2,9 @@ import Data.List
 import System.IO
 import Data.Char(digitToInt)
 
-
-type Counter      = Int
-type Binary       = [Int]
-type Accumulator  = [Int]
+type Binary = [Int]
+type Accumulator = [Int]
+type Counter = Int
 
 bin' :: Binary -> Counter -> Accumulator -> Int
 bin' []     _ acc = sum acc
@@ -21,48 +20,79 @@ bin xs = bin' reversedList 0 []
     reversedList = reverse xs
 -----
 
-convert :: String -> [Int]
-convert xs = map (\x -> digitToInt x) xs
+type Stats = (Int, Int)
+type Report = [Int]
+
+strToIntArray :: String -> Report
+strToIntArray xs = map (\x -> digitToInt x) xs
+
+-- functions to capture the frequency of 1s and 0s
+frequency :: Report -> Stats -> Stats
+frequency [] acc      = acc
+frequency (x:xs) (on,off)
+  | x == 1    = frequency xs (on+1, off)
+  | otherwise = frequency xs (on, off+1)
+
+statsIdentity :: Stats
+statsIdentity = (0,0)
+
+increment :: Stats -> Int -> Stats
+increment (x,y) d = if d == 1 then (x+1,y) else (x,y+1)
+
+stats :: Report -> Stats
+stats xs = foldl increment statsIdentity xs
+
+allStats :: [Report] -> [Stats]
+allStats xs = map stats (transpose xs)
 
 
-rate rSize rSum = if isOn then 1 else 0
-  where 
-    half  = div rSize 2
-    isOn  = rSum > half
+-- calculating bit criteria which returns which bit to consider valid for a given position in a binary number
+data RatingType = Oxygen | CO2
 
-
-gammaRate :: Int -> [Int] -> [Int]
-gammaRate rSize xs = map (\x -> (rate rSize x)) xs
-
-
-sumVectors :: [Int] -> [Int] -> [Int] -> [Int]
-sumVectors [] [] acc          = acc
-sumVectors (x:xs) (y:ys) acc  = sumVectors xs ys (acc ++ [x + y])
-
-
-scan :: [[Int]] -> [Int] -> [Int]
-scan [] acc           = acc
-scan (xs':xs) acc     = scan xs newAcc
-  where 
-    newAcc  = sumVectors xs' acc []
-
-
-findGammaRate :: [[Int]] -> [Int]
-findGammaRate xs = gammaRate (length xs) listCount
+bitCriteria :: Stats -> RatingType -> Int
+bitCriteria (x,y) t = if x == y then result else maxMin
   where
-    lbin      = length (head xs)
-    listCount = scan xs (replicate lbin 0)
+    result = case t of
+      Oxygen -> 1
+      CO2    -> 0
+    maxMin = case t of
+      Oxygen -> if x > y then 1 else 0
+      CO2    -> if x > y then 0 else 1
 
+
+-- predicates for each rating type
+pOxygen :: Int -> Report -> Stats -> Bool
+pOxygen pos r s = bit == (bitCriteria s Oxygen)
+  where bit = r !! pos
+
+pCO2 :: Int -> Report -> Stats -> Bool
+pCO2 pos r s = bit == (bitCriteria s CO2)
+  where bit = r !! pos
+
+filterOxygen xs sts  = filter' pOxygen 0 xs sts
+filterCO2    xs sts  = filter' pCO2 0 xs sts
+
+oxygenGenRate xs sts = bin $ filterOxygen xs sts
+co2GenRate xs sts    = bin $ filterCO2 xs sts
+
+filter' :: (Int -> Report -> Stats -> Bool) -> Int -> [Report] -> [Stats] -> Report
+filter' _ _ [x] _ = x
+filter' f pos rs (s:stats) = filter' f (pos+1) rs' stats
+  where
+    rs' = filter (\r -> (f pos r s)) rs
+
+gammaRate stats   = bin $ map (\s -> if (fst s) > (snd s) then 1 else 0) stats
+epsilonRate stats = bin $ map (\s -> if (fst s) < (snd s) then 1 else 0) stats
+    
 
 main = do
     contents <- readFile "day_03.input"
-    let xs   = map convert (words contents)
+    let xs   = map strToIntArray (words contents)
     let lbin = length (head xs)
-
-    putStrLn "solution 1"
-    let gamma   = findGammaRate xs
-    let epsilon = map (\x -> if x == 1 then 0 else 1) gamma
-    print $ bin gamma * bin epsilon
+    let stats = allStats xs
     
-
+    putStrLn "solution 1"
+    print $ (gammaRate stats) * (epsilonRate stats)        
+    putStrLn "solution 2"
+    print $ (oxygenGenRate xs stats) * (co2GenRate xs stats) 
 
